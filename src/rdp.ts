db7625bdf84d5833a16f7accae59bab45e856ba9
d180cf <d180cf@gmail.com>
2015-07-03 19:18:09 -0700

    Move the general parsing logic to rdp.

---
+++ src/rdp.ts
@@ -0,0 +1,86 @@
+﻿module SDP {
+    export class Pattern<T> {
+        constructor(private _text, private _exec) {
+
+        }
+
+        toString() {
+            return this._text;
+        }
+
+        exec(str: string, pos: number): [T, number];
+        exec(str: string): T;
+
+        exec(str, pos?) {
+            const r = this._exec(str, pos || 0);
+            return typeof pos === 'number' ? r :
+                r[1] == str.length ? r[0] :
+                    null;
+        }
+
+        map<U>(fn: (value: T) => U): Pattern<U> {
+            return $(this + '', (str, pos) => {
+                const r = this.exec(str, pos);
+                return r ? [fn(r[0]), r[1]] : null;
+            });
+        }
+    }
+
+    export function $<T>(exec: Pattern<T>): Pattern<T>;
+    export function $<T>(text: string, exec: (str: string, pos: number) => [T, number]): Pattern<T>;
+    export function $(regexp: RegExp): Pattern<string>;
+    export function $(string: string): Pattern<string>;
+    export function $(items: any[]): Pattern<any[]>;
+    export function $(pattern: any, min: number): Pattern<any[]>;
+
+    export function $(x, n?): Pattern<any> {
+        if (x instanceof Pattern) return x;
+        if (typeof x === 'function') return new Pattern(n, x);
+        if (x instanceof RegExp) return rgx(x);
+        if (typeof x === 'string') return txt(x);
+        if (x instanceof Array) return seq(...x);
+        if (typeof x === 'number') return rep(x, n);
+    }
+
+    function rgx(r: RegExp) {
+        return $(r + '', (str, pos) => {
+            const m = r.exec(str.slice(pos));
+            return m && m.index == 0 ? [m[0], pos + m[0].length] : null;
+        });
+    }
+
+    function txt(s: string) {
+        return $('"' + s + '"', (str, pos) => {
+            return str.slice(pos, pos + s.length) == s ? [s, pos + s.length] : null;
+        });
+    }
+
+    function seq(...ps: any[]) {
+        return $(ps.join(' '), (str, pos) => {
+            const res = [];
+
+            for (const p of ps) {
+                const r = $(p).exec(str, pos);
+                if (!r) return null;
+                res.push(r[0]);
+                pos = r[1];
+            }
+
+            return [res, pos];
+        });
+    }
+
+    function rep(p, min = 0) {
+        return $(min + '*' + p, (str, pos) => {
+            const res = [];
+            let r: [any, number];
+
+            while (r = $(p).exec(str, pos)) {
+                res.push(r[0]);
+                pos = r[1];
+            }
+
+            return res.length >= min ? [res, pos] : null;
+        });
+    }
+}
--- src/sgf.ts
+++ src/sgf.ts
@@ -1,4 +1,6 @@
-﻿module SGF {
+﻿/// <reference path="rdp.ts" />
+
+module SGF {
     /** Tag AW[bb][cb][cc] gets decomposed into:
      *
      *      [name] = AW
@@ -24,96 +26,25 @@
         [variationIndex: number]: Node;
     }
 
-    interface E<T> {
-        (str: string, pos: number): [T, number];
-    }
-
-    class Pattern<T> {
-        constructor(public exec: E<T>) {
-
-        }
-
-        map<U>(fn: (value: T) => U) {
-            return new Pattern((str, pos) => {
-                const r = this.exec(str, pos);
-                return r ? [fn(r[0]), r[1]] : null;
-            });
-        }
-    }
-
-    function $<T>(exec: Pattern<T>): Pattern<T>;
-    function $<T>(exec: E<T>): Pattern<T>;
-    function $(regexp: RegExp): Pattern<string>;
-    function $(string: string): Pattern<string>;
-    function $(items: any[]): Pattern<any[]>;
-    function $(pattern: any, min: number): Pattern<any[]>;
-
-    function $(x, n?): Pattern<any> {
-        if (x instanceof Pattern) return x;
-        if (typeof x === 'function') return new Pattern(x);
-        if (x instanceof RegExp) return rgx(x);
-        if (typeof x === 'string') return txt(x);
-        if (x instanceof Array) return seq(...x);
-        if (typeof x === 'number') return rep(x, n);
-    }
-
-    function rgx(r: RegExp) {
-        return $((str, pos) => {
-            const m = r.exec(str.slice(pos));
-            return m && m.index == 0 ? [m[0], pos + m[0].length] : null;
-        });
-    }
-
-    function txt(s: string) {
-        return $((str, pos) => {
-            return str.slice(pos, pos + s.length) == s ? [s, pos + s.length] : null;
-        });
-    }
-
-    function seq(...ps: any[]) {
-        return $((str, pos) => {
-            const res = [];
-
-            for (const p of ps) {
-                const r = $(p).exec(str, pos);
-                if (!r) return null;
-                res.push(r[0]);
-                pos = r[1];
-            }
-
-            return [res, pos];
-        });
-    }
-
-    function rep(p, min) {
-        return $((str, pos) => {
-            const res = [];
-            let r: [any, number];
-
-            while (r = $(p).exec(str, pos)) {
-                res.push(r[0]);
-                pos = r[1];
-            }
-
-            return res.length >= min ? [res, pos] : null;
-        });
-    }
+    import $ = SDP.$;
 
     function parse(source: string): Node {
-        const val = $(/\[.*?\]/).map(s => s.slice(+1, -1));
+        var val = $(/\[.*?\]/).map(s => s.slice(+1, -1));
 
-        const tag = $([/\s*;/, /\w+/, $(val, 0)]).map(r => {
+        var tag = $([/\s*;/, /\w+/, $(val, 0)]).map(r => {
             const t: Tag = r[3];
             t.name = r[2];
             return t;
         });
 
-        const sgf = $([/\s*\(/, $(tag, 0), $((s, i) => sgf.exec(s, i), 0), ')']).map(r => {
+        var sub = $('sgf', (s, i) => sgf.exec(s, i));
+
+        var sgf = $([/\s*\(/, $(tag, 0), $(sub, 0), ')']).map(r => {
             const n: Node = r[3];
             n.tags = r[2];
             return n;
         });
 
-        return sgf.exec(source, 0)[0];
+        return sgf.exec(source);
     }
 }
