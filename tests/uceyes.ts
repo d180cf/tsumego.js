e7ced9ac6058bf3fb689bd104350251f92f5bd5e
d180cf <d180cf@gmail.com>
2015-07-05 23:00:46 -0700

    Improved tests for Pattern.isEye

--- tests/uceyes.ts
+++ tests/uceyes.ts
@@ -1,47 +1,44 @@
-/// <reference path="ut.ts" />
-/// <reference path="../tsumego.d.ts" />
-
 ut.group($ => {
     $.test($ => {
-        const board = new Board(8, [
-            '.X....O.',
-            'XXXX..OO',
-            '.X.X..O.',
-            'XXXX..OO',
-            '.XOOOO..',
-            '.O.O.O..',
-            'OOOOO.XX',
-            '.O.O..X.'
-        ]);
+        const board = new Board(`
+            (;FF[4]SZ[16]
+                AB
+                [ad][ac][ab][bm][bd][bb][ba][cm][ce][cd][cc][do]
+                [dn][dm][de][dc][ee][ed][ec][hk][hj][hi][hf][he]
+                [hd][hc][ik][ii][ig][ie][ic][jk][jj][ji][jg][jf]
+                [je][jd][mb][ma][no][nb][op][oo][oi][oh][po][pi]
+
+                AW
+                [ao][bp][bo][bn][co][fm][fl][gn][gl][gk][hm][im]
+                [il][md][nf][ne][nd][og][of][od][oc][ob][oa][pg]
+                [pe][pc][pb])
+        `);
 
         const eyes = {};
 
-        for (let x = 0; x < 8; x++) {
-            for (let y = 0; y < 8; y++) {
-                let t = '';
+        for (let x = 0; x < 16; x++) {
+            for (let y = 0; y < 16; y++) {
+                // I is skipped
+                const xy = String.fromCharCode(x > 7 ? x + 0x42 : x + 0x41) + (16 - y);
 
                 if (Pattern.isEye(board, x, y, +1))
-                    t += 'X';
+                    eyes[xy] = +1;
 
                 if (Pattern.isEye(board, x, y, -1))
-                    t += 'O';
-
-                if (t)
-                    eyes[x + ':' + y] = t;
+                    eyes[xy] = -1;
             }
         }
 
         $(eyes).equal({
-            '0:0': 'X',
-            '7:0': 'O',
-            '0:2': 'X',
-            '2:2': 'X',
-            '7:2': 'O',
-            '2:5': 'O',
-            '0:7': 'O',
-            '2:7': 'O',
-            '4:5': 'O',
-            '7:7': 'X'
+            A16: +1,
+            B14: +1,
+            D13: +1,
+            J13: +1,
+            J11: +1,
+            J7: +1,
+            Q1: +1,
+            Q16: -1,
+            A1: -1
         });
     });
 });
