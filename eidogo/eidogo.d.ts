4bff6ec3775fbf0fde808cac1568c58ef637cf54
d180cf <d180cf@gmail.com>
2015-07-18 15:14:24 -0700

    Updates in the EidoGo interface.

--- eidogo/eidogo.d.ts
+++ eidogo/eidogo.d.ts
@@ -2,6 +2,13 @@ declare module eidogo {
     export interface Player {
         new (options);
 
+        unsavedChanges: boolean;
+        cursor: Cursor;
+
+        /** A mysterious method which needs to be called
+            after adding a comment for the current node :) */
+        refresh(): void;
+
         /** "fb" means x = 5, y = 1 */
         createMove(xy: string);
 
@@ -10,6 +17,17 @@ declare module eidogo {
 
         /** undo the last move */
         back();
+    }
+
+    /** Points to the current SGF node? */
+    export interface Cursor {
+        node: Node;
+    }
+
+    /** Represents an SGF node? */
+    export interface Node {
+        /** C{Good for W] */
+        C: string;
     }
 
     export interface Static {
