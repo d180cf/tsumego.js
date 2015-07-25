e4f83ff7d98bd791e28826a63d33031817cef208
d180cf <d180cf@gmail.com>
2015-07-25 14:37:28 -0700

    Move the http server to /testbench

--- server.js
+++
@@ -1,5 +0,0 @@
-﻿var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = +process.argv[2] || 8080;
-connect().use(serveStatic(__dirname)).listen(port);
-console.log('Listening on port', port);
