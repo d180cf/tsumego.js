e8bf7943b0011f5c1867d3f68162cafdeb613c06
d180cf <d180cf@gmail.com>
2015-07-25 20:59:35 -0700

    Infra for solver tests.

--- tests/solver.ts
+++ tests/solver.ts
@@ -1,42 +1,85 @@
 /// <reference path="infra.ts" />
 
 module tests {
-    function solve(g: Graph, c: number, k: number) {
-
-    }
-
-    /** 70204 means:
-     *  - 2 b stones
-     *  - 4 w stones
-     *  - 7 is a tag */
-    type Node = number;
+    /** 
+     * [ number of b stones,
+     *   number of w stones,
+     *   unique tag ]
+     */
+    type Hash =[number, number, number];
 
     /** +1 - b wins
      *  -1 - w wins
      *   0 - the status isn't clear */
     type Status = number;
 
-    class Graph {
-        constructor(root: Node, make: (add: Graph.Builder) => void) {
+    type Node = tsumego.Hasheable;
 
+    class Graph {
+        root: Node = Graph.Node('x,x,777');
+        nodes: { [hash: string]: Node } = {};
+        moves: { [hash: string]: Status|string[] } = {};
+
+        constructor(build: (add: Graph.Builder) => void) {
+            const node = this.root;
+
+            build((hash, next) => {
+                this.nodes[hash] = this.nodes[hash] || Graph.Node(hash + '');
+                
+            });
+        }
+
+        solve(p: Hash[], color: number, nkt: number) {
+            return tsumego.solve(
+                !p ? [this.root] : p.map(h => this.nodes[h + '']),
+                color,
+                nkt,
+                new tsumego.TT,
+                b => this.generate(b),
+                b => this.estimate(b));
+        }
+
+        generate(b: Node) {
+            const ms = this.moves[b.hash()];
+
+            if (typeof ms === 'number') {
+                throw Error(b.hash() + ' has a known status');
+            } else {
+                return ms.map(h => {
+                    const tag = +h.split(',')[2];
+
+                    return {
+                        b: this.nodes[h],
+                        m: { x: tag, y: tag }
+                    };
+                });
+            }
+        }
+
+        estimate(b: Node) {
+            const s = this.moves[b.hash()];
+            return typeof s === 'number' ? s : 0;
         }
     }
 
     module Graph {
         export interface Builder {
-            (node: Node, vars: Builder): void;
-            (node: Node, status: Status): void;
+            (hash: Hash, status: Status): void;
+            (hash: Hash, build?: Builder): void;
         }
+
+        export const Node = (hash: string) => ({ hash: () => hash });
     }
 
     ut.group($ => {
         $.test($ => {
-            const g = new Graph(10000, add => {
-                add(20100, +1);
-                add(20100, -1);
+            const g = new Graph(add => {
+                add([1, 0, 111], +1);
+                add([1, 0, 222], -1);
             });
 
-            const r = solve(g, +1, 0);
+            $(g.solve(null, +1, 0)).equal({ color: +1, move: { x: 111, y: 111 } });
+            $(g.solve(null, -1, 0)).equal({ color: -1, move: { x: 222, y: 222 } });
         });
     });
 }
