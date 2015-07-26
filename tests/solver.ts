986c11d671c837e752ac973d174c62d7e2c33870
d180cf <d180cf@gmail.com>
2015-07-25 23:21:13 -0700

    Infra for the solver tests.

--- tests/solver.ts
+++ tests/solver.ts
@@ -15,17 +15,46 @@ module tests {
 
     type Node = tsumego.Hasheable;
 
-    class Graph {
-        root: Node = Graph.Node('x,x,777');
-        nodes: { [hash: string]: Node } = {};
-        moves: { [hash: string]: Status|string[] } = {};
+    class GameGraph {
+        root: Node;
 
-        constructor(build: (add: Graph.Builder) => void) {
-            const node = this.root;
+        private nodes: { [hash: string]: Node } = {};
+        private moves: { [hash: string]: string[] } = {};
+        private state: { [hash: string]: Status } = {};
 
-            build((hash, next) => {
-                this.nodes[hash] = this.nodes[hash] || Graph.Node(hash + '');
-                
+        constructor(build: GameGraph.Builder) {
+            const hash = 'x,x,777';
+            this.init(hash, 0, build);
+            this.root = this.nodes[hash];
+        }
+
+        private init(hash: string, status: Status, build: GameGraph.Builder) {
+            const moves = [];
+
+            this.nodes[hash] = new GameGraph.Node(hash);
+            this.moves[hash] = moves;
+            this.state[hash] = status;
+
+            if (!build)
+                return;
+
+            build((h, s, b) => {
+                if (moves.indexOf(h + '') >= 0)
+                    throw Error(h + ' already added');
+
+                if ((h + '') in this.nodes) {
+                    // adding a link to an existing node
+                    // which creates a loop in the graph
+                    if (s !== void 0 || b !== void 0)
+                        throw Error(h + ' already has a status');
+
+                    moves.push(h + '');
+                } else {
+                    if (s === void 0)
+                        throw Error(h + ' is a new node and must have a status');
+
+                    this.init(h + '', s, b);
+                }
             });
         }
 
@@ -35,45 +64,61 @@ module tests {
                 color,
                 nkt,
                 new tsumego.TT,
-                b => this.generate(b),
+                (b, c) => this.generate(b, c),
                 b => this.estimate(b));
         }
 
-        generate(b: Node) {
-            const ms = this.moves[b.hash()];
+        generate(b: Node, c: number) {
+            const h0 = b.hash();
+            const [nb0, nw0] = h0.split(',');
+            const ms = this.moves[h0];
 
             if (typeof ms === 'number') {
                 throw Error(b.hash() + ' has a known status');
             } else {
                 return ms.map(h => {
-                    const tag = +h.split(',')[2];
+                    const [nb, nw, tag] = h.split(',');
 
                     return {
                         b: this.nodes[h],
-                        m: { x: tag, y: tag }
+                        c: [+nb, +nw],
+                        m: { x: +tag, y: +tag }
                     };
+                }).filter(w => {
+                    return c > 0 ?
+                        w.c[0] > +nb0 :
+                        w.c[1] > +nw0;
                 });
             }
         }
 
-        estimate(b: Node) {
-            const s = this.moves[b.hash()];
-            return typeof s === 'number' ? s : 0;
+        estimate(b: Node): Status {
+            const h = b.hash();
+            const s = this.state[h];
+            if (typeof s !== 'number')
+                throw Error(h + ' doesnt have a status');
+            return s;
         }
     }
 
-    module Graph {
+    module GameGraph {
         export interface Builder {
-            (hash: Hash, status: Status): void;
-            (hash: Hash, build?: Builder): void;
+            (add: (hash: Hash, status: Status, build?: Builder) => void): void;
         }
 
-        export const Node = (hash: string) => ({ hash: () => hash });
+        export class Node implements tsumego.Hasheable {
+            constructor(private _hash: string) {
+            }
+
+            hash() {
+                return this._hash;
+            }
+        }
     }
 
     ut.group($ => {
         $.test($ => {
-            const g = new Graph(add => {
+            const g = new GameGraph(add => {
                 add([1, 0, 111], +1);
                 add([1, 0, 222], -1);
             });
