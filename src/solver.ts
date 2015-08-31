1ac98e9b5556dd522a9893e186e2e5c1051ae2e6
d180cf <d180cf@gmail.com>
2015-08-30 20:34:47 -0700

    + Array.from polyfill, - the old solver impl

--- src/solver.ts
+++ src/solver.ts
@@ -29,168 +29,6 @@ module tsumego {
                 return i;
     }
 
-    export class _Solver<Node extends Hasheable, Move> {
-        private path: Node[] = [];
-
-        private tags: {
-            res?: Result<Move>;
-            color: Color;
-            nkt: number;
-            move?: Move;
-            passed?: boolean;
-            next?: { node: Node, move: Move, nkt: number }[];
-            mindepth?: number;
-        }[] = [];
-
-        get depth() {
-            return this.path.length;
-        }
-
-        get current() {
-            const i = this.depth - 1;
-            return { tag: this.tags[i], node: this.path[i] };
-        }
-
-        constructor(path: Node[], color: Color, nkt: number,
-            private tt: TT<Move>,
-            private expand: Generator<Node, Move>,
-            private status: Estimator<Node>,
-            private player?: Player<Move>) {
-
-            for (const b of path) {
-                this.path.push(b);
-                this.tags.push(null);
-            }
-
-            this.tags[this.tags.length - 1] = {
-                color: color,
-                nkt: nkt
-            };
-        }
-
-        next(): void {
-            const {tag: t, node} = this.current;
-            const {res, color, nkt} = t;
-
-            if (res) {
-                this.exit();
-                return;
-            }
-
-            if (!t.next) {
-                const leafs = this.expand(node, color);
-
-                t.mindepth = infty;
-                t.next = [];
-
-                for (const {b, m} of leafs) {
-                    const d = findrepd(this.path, b);
-                    const ko = d < this.depth;
-
-                    if (d < t.mindepth)
-                        t.mindepth = d;
-
-                    // the move makes sense if it doesn't repeat
-                    // a previous position or the current player
-                    // has a ko treat elsewhere on the board and
-                    // can use it to repeat the local position
-                    if (!ko || color * nkt > 0) {
-                        t.next.push({
-                            node: b,
-                            move: m,
-                            nkt: ko ? nkt - color : nkt
-                        });
-                    }
-                }
-
-                // moves that require a ko treat are considered last
-                // that's not just perf optimization: the search depends on this
-                t.next.sort((lhs, rhs) => (rhs.nkt - lhs.nkt) * color);
-
-                // nodes will be .pop()'ed,
-                // so reversing the list
-                t.next.reverse();
-            }
-
-            this.pick();
-        }
-
-        private pick(): void {
-            const {tag: {color, nkt, passed, move, next}, node} = this.current;
-
-            if (next.length > 0) {
-                const {move, nkt, node} = next.pop();
-                this.play(node, move, color, nkt);
-            } else if (passed) {
-                this.done({ color: -color, move: move }, Color.alias(color) + ' is out of moves');
-            } else {
-                const prev = this.tags[this.depth - 2];
-
-                if (prev && prev.passed) {
-                    this.done({ color: this.status(node) }, 'both passed');
-                } else {
-                    this.current.tag.passed = true;
-                    this.play(node, null, color, nkt);
-                }
-            }
-        }
-
-        private play(node: Node, move: Move, color: Color, nkt: number): void {
-            const ttres = this.tt.get(node, -color, nkt);
-
-            if (ttres) {
-                if (ttres.color * color > 0)
-                    this.done(ttres, 'cached result');
-                else if (this.player)
-                    this.player.loss(color, move, ttres.move);
-            } else if (this.status(node) > 0) {
-                this.done({ color: +1, move: move });
-            } else {
-                this.path.push(node);
-                this.tags.push({ color: -color, nkt: nkt, move: move });
-
-                if (this.player)
-                    this.player.play(color, move);
-            }
-        }
-
-        private done(r: Result<Move>, comment?: string) {
-            this.current.tag.res = r;
-
-            if (this.player)
-                this.player.done(r.color, r.move, comment);
-        }
-
-        private exit(): void {
-            const {tag: {color, nkt, mindepth, res}, node} = this.current;
-
-            if (color * res.color < 0)
-                res.repd = mindepth;
-
-            if (!res.repd || res.repd > this.depth)
-                this.tt.set(node, color, res, nkt);
-
-            this.path.pop();
-            this.tags.pop();
-
-            if (this.player) {
-                const move = res.move;
-                this.done(res);
-                this.player.undo();
-            }
-
-            if (!this.current)
-                return;
-
-            if (this.current.tag.color * res.color > 0) {
-                this.done(res);
-                return;
-            }
-
-            this.pick();
-        }
-    }
-
     function best<Move>(s1: Result<Move>, s2: Result<Move>, c: Color) {
         let r1 = s1 && s1.color;
         let r2 = s2 && s2.color;
@@ -214,6 +52,13 @@ module tsumego {
     }
 
     const wins = (color: number, result: number) => color * result > 0;
+
+    Array.from = Array.from || function (iterable) {
+        const array = [];
+        for (const item of iterable)
+            array.push(item);
+        return array;
+    };
 
     export function* Solver<Node extends Hasheable, Move>(
         path: Node[],
