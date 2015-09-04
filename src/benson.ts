608277cdb014c49225c25d4714ce11bceab9753e
d180cf <d180cf@gmail.com>
2015-09-03 22:08:29 -0700

    Simplified the implementation again.

--- src/benson.ts
+++ src/benson.ts
@@ -10,30 +10,40 @@
 module tsumego.benson {
     'use strict';
 
-    const merge = (x: number, y: number) => x | (y << 8);
-    const split = (xy: number): XY => ({ x: xy & 256, y: xy >> 8 });
+    function contains(coords: XY[], {x, y}: XY) {
+        for (const s of coords)
+            if (x == s.x && y == s.y)
+                return true;
+        return false;
+    }
 
     function* region(root: XY, belongs: (target: XY, source: XY) => boolean) {
-        const body: number[] = [];
-        const edge = [merge(root.x, root.y)];
+        const body: XY[] = [];
+        const edge = [root];
 
         while (edge.length > 0) {
             const xy = edge.pop();
-            yield split(xy);
+
+            yield xy;
             body.push(xy);
 
-            for (const nxy of [xy - 1, xy + 1, xy - 0x100, xy + 0x100]) {
-                if (body.indexOf(nxy) >= 0) continue;
-                if (edge.indexOf(nxy) >= 0) continue;
-                if (!belongs(split(nxy), split(xy))) continue;
+            const {x, y} = xy;
 
-                edge.push(nxy);
-            }
+            const neighbors = [
+                { x: x - 1, y: y },
+                { x: x + 1, y: y },
+                { x: x, y: y - 1 },
+                { x: x, y: y + 1 }
+            ];
+
+            for (const nxy of neighbors)
+                if (belongs(nxy, xy) && !contains(body, nxy) && !contains(edge, nxy))
+                    edge.push(nxy);
         }
     }
 
     /** A region is vital to a chain if all its empty intersections are liberties of that chain. */
-    function isVital(board: Board, region: Iterable<XY>, liberties: Iterable<XY>) {
+    function isVital(board: Board, region: Iterable<XY>, liberties: XY[]) {
         search: for (const {x, y} of region) {
             if (board.at(x, y))
                 continue;
@@ -51,21 +61,22 @@ module tsumego.benson {
     /** A group of stones is unconditionally alive if there are
         two regions in which all vacant intersections are liberties
         of the group. Such regions are called "eyes" of the group. */
-    export function alive(board: Board, roots: XY[]) {
-        const color = board.at(roots[0].x, roots[0].y);
+    export function alive(b: Board, roots: XY[]) {
+        const color = b.at(roots[0].x, roots[0].y);
+        const sameColor = (s: XY) => b.at(s.x, s.y) * color > 0;
         const liberties: XY[] = [];
 
         for (const root of roots)
-            for (const s of region(root, (target, source) => board.at(source.x, source.y) * color > 0))
-                if (!board.at(s.x, s.y))
+            for (const s of region(root, (target, source) => sameColor(source)))
+                if (!b.at(s.x, s.y))
                     liberties.push(s);
 
         let eyes = 0;
 
         for (const lib of liberties) {
-            const r = region(lib, s => board.at(s.x, s.y) * color <= 0);
+            const adjacent = region(lib, (target, source) => !sameColor(target));
 
-            if (!isVital(board, r, liberties))
+            if (!isVital(b, adjacent, liberties))
                 continue;
 
             if (++eyes > 1)
