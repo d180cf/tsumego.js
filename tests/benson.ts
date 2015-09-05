b1d5da63dedc60db5295c7f05da5a3e7b1e135c8
d180cf <d180cf@gmail.com>
2015-09-05 14:29:02 -0700

    A few tests for the benson's analyser.
    This test is failing because the current implementation tries to use a shortcut:
    
    ```
    X - - X
    - X X X
    X X X X
    ```
    
    It sees that the bigger group has two vital regions, but this doesn't mean that the group is alive.

--- tests/benson.ts
+++ tests/benson.ts
@@ -26,10 +26,54 @@ module test {
         });        
 
         $.test($ => {
+            /// one vital region is not enough
+            const b = new Board(9, [
+                '--X',
+                '--X',
+                'XXX'
+            ]);
+            const r = benson.alive(b, [new XY(2, 2)]);
+            $(r).equal(false);
+        });
+
+        $.test($ => {
             /// a two-eye group is alive
             const b = new Board(9, ['-X-X', 'XXXX']);
             const r = benson.alive(b, [new XY(1, 1)]);
             $(r).equal(true);
+        });
+
+        $.test($ => {
+            /// two vital regions is enough
+            const b = new Board(9, [
+                '--X--X',
+                '--X--X',
+                'XXXXXX'
+            ]);
+            const r = benson.alive(b, [new XY(2, 2)]);
+            $(r).equal(true);
+        });
+
+        $.test($ => {
+            /// the smallest alive group
+            const b = new Board(9, [
+                'X-X',
+                '-XX',
+                'XXX'
+            ]);
+            const r = benson.alive(b, [new XY(2, 2)]);
+            $(r).equal(true);
+        });
+
+        $.test($ => {
+            /// not pass-alive due to a ko
+            const b = new Board(9, [
+                'X--X',
+                '-XXX',
+                'XXXX'
+            ]);
+            const r = benson.alive(b, [new XY(2, 2)]);
+            $(r).equal(false);
         });
     });
 }
