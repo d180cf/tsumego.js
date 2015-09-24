02dcfc19b4f2c58f792d4cd141a70adb3689c1e0
d180cf <d180cf@gmail.com>
2015-09-23 22:11:41 -0700

    +1 test

--- tests/ann.ts
+++ tests/ann.ts
@@ -7,7 +7,7 @@ module test {
         /// ann
 
         $.test($ => {
-            /// simple 2:2:1 net
+            /// simple 2:2:1 net #1
 
             const net = new Net(2);
 
@@ -29,6 +29,31 @@ module test {
 
             const output2 = net.apply(input);
             $(output2).equal([0.6820185832642942]);
+        });
+
+        $.test($ => {
+            /// simple 2:2:1 net #2
+
+            const net = new Net(2);
+
+            net.add([
+                [0.1, 0.5],
+                [0.3, 0.2]
+            ]);
+
+            net.add([
+                [0.2, 0.1]
+            ]);
+
+            const input = [0.1, 0.7];
+
+            const output1 = net.apply(input);
+            $(output1).equal([0.5429061854494511]);
+
+            net.adjust([1.0]);
+
+            const output2 = net.apply(input);
+            $(output2).equal([0.5609479467866498]);
         });
     });
 }
