ba6abb304fb5f3d36ee0239dcf92f0b09f5d91fd
d180cf <d180cf@gmail.com>
2015-09-23 21:19:24 -0700

    Move vector/matrix functions to a new linalg module.

--- src/ann.ts
+++ src/ann.ts
@@ -1,60 +1,18 @@
+/// <reference path="linalg.ts" />
+
 /**
  * Artificial Neural Networks.
  *
  * https://en.wikipedia.org/wiki/Backpropagation
  */
 module tsumego.ann {
+    'use strict';
+
+    import vector = linalg.vector;
+    import matrix = linalg.matrix;
+
     const sigmoid0 = (x: number) => 1 / (1 + Math.exp(-x)); // S(x) = 1/(1 + 1/e**x)
     const sigmoid1 = (s: number) => s * (1 - s); // d/dx S(x) = S(x) * (1 - S(x))
-
-    type vector = number[];
-    type matrix = vector[];
-
-    /** [for (i = 0; i < n; i++) f(i)] */
-    const from = <T>(n: number, f: (i: number) => T) => {
-        const a: T[] = new Array(n);
-
-        for (let i = 0; i < n; i++)
-            a[i] = f(i);
-
-        return a;
-    };
-
-    /** vector dot product */
-    const vdot = (u: vector, v: vector) => {
-        let s = 0;
-
-        for (let i = 0; i < u.length; i++)
-            s += u[i] * v[i];
-
-        return s;
-    };
-
-    /** vector dyad product */
-    const dyad = (u: vector, v: vector) =>
-        from(u.length, i =>
-            from(v.length, j => u[i] * v[j]));
-
-    /** w[i] = u[i] * v[i] */
-    const dot2 = (u: vector, v: vector) =>
-        from(u.length, i => u[i] * v[i]);
-
-    /** matrix by vector multiplication */
-    const mulv = (m: matrix, v: vector) =>
-        from(m.length, i => vdot(m[i], v));
-
-    /** u + k * v */
-    const vsum = (u: vector, v: vector, k = 1) =>
-        from(u.length, i => u[i] + k * v[i]);
-
-    /** a + k * b */
-    const msum = (a: matrix, b: matrix, k = 1) =>
-        from(a.length, i => vsum(a[i], b[i], k));
-
-    /** matrix transposition */
-    const mtrs = (m: matrix) =>
-        from(m[0].length, i =>
-            from(m.length, j => m[i][j]));
 
     /**
      * The simplest layered ANN.
@@ -91,7 +49,7 @@ module tsumego.ann {
             vs[0] = input;
 
             for (let i = 0; i < ws.length; i++)
-                vs[i + 1] = mulv(ws[i], vs[i]).map(sigmoid0); // vs[i+1] = ws[i]*vs[i] | f'
+                vs[i + 1] = matrix.mulv(ws[i], vs[i]).map(sigmoid0); // vs[i+1] = ws[i]*vs[i] | f'
         }
 
         /**
@@ -118,14 +76,23 @@ module tsumego.ann {
             const ws = this.layers;
 
             const v0 = vs[vs.length - 1];
-            let d0 = dot2(vsum(v0, target, -1), v0.map(sigmoid1)); // d[n] = (vs[n] - t) : (vs[n] | f')
+
+            // d[n] = (vs[n] - t) : (vs[n] | f')
+            let d = vector.dot2(
+                vector.sum(v0, target, -1),
+                v0.map(sigmoid1));
 
             for (let i = ws.length - 1; i >= 0; i--) {
                 const w = ws[i];
                 const v = vs[i];
 
-                ws[i] = msum(w, dyad(d0, v), -k); // dw[i] = -k * dyad(d[i + 1], v[i])
-                d0 = dot2(mulv(mtrs(w), d0), v.map(sigmoid1)); // d[i] = (w[i]^T * d[i + 1]) : (v[i] | f')
+                // dw[i] = -k * dyad(d[i + 1], v[i])
+                ws[i] = matrix.sum(w, vector.dyad(d, v), -k);
+
+                // d[i] = (w[i]^T * d[i + 1]) : (v[i] | f')
+                d = vector.dot2(
+                    matrix.mulv(matrix.transpose(w), d),
+                    v.map(sigmoid1));
             }
         }
     }
---
+++ src/linalg.ts
@@ -0,0 +1,61 @@
+module tsumego.linalg {
+    'use strict';
+
+    export interface vector {
+        [i: number]: number;
+        length: number;
+        map(f: (x: number) => number): vector;
+    }
+
+    export interface matrix {
+        [row: number]: vector;
+        length: number;
+    }
+
+    const from = <T>(n: number, f: (i: number) => T) => {
+        const a: T[] = new Array(n);
+
+        for (let i = 0; i < n; i++)
+            a[i] = f(i);
+
+        return a;
+    };
+
+    export module vector {
+        export const dot = (u: vector, v: vector) => {
+            let s = 0;
+
+            for (let i = 0; i < u.length; i++)
+                s += u[i] * v[i];
+
+            return s;
+        };
+
+        /** m[i][j] = u[i] * v[j] */
+        export const dyad = (u: vector, v: vector): matrix =>
+            from(u.length, i =>
+                from(v.length, j => u[i] * v[j]));
+
+        /** w[i] = u[i] * v[i] */
+        export const dot2 = (u: vector, v: vector): vector =>
+            from(u.length, i => u[i] * v[i]);
+
+        /** u + k * v */
+        export const sum = (u: vector, v: vector, k = 1): vector =>
+            from(u.length, i => u[i] + k * v[i]);
+    }
+
+    export module matrix {
+        /** m * v */
+        export const mulv = (m: matrix, v: vector): vector =>
+            from(m.length, i => vector.dot(m[i], v));
+
+        /** a + k * b */
+        export const sum = (a: matrix, b: matrix, k = 1): matrix =>
+            from(a.length, i => vector.sum(a[i], b[i], k));
+
+        export const transpose = (m: matrix): matrix =>
+            from(m[0].length, i =>
+                from(m.length, j => m[i][j]));
+    }
+}
