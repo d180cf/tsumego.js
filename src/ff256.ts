55ee381d8f93d418a2b62f9d0b8390230f0b69e2
d180cf <d180cf@gmail.com>
2015-10-16 23:49:30 -0700

    tests for ff256

--- src/ff256.ts
+++ src/ff256.ts
@@ -5,7 +5,7 @@
  * www.cs.utsa.edu/~wagner/laws/FFM.html
  */
 namespace tsumego.ff256 {
-    const mul3 = (x: number) => x ^ (x & 0x80 ? x ^ 0x8d : x) << 1; // x * 3
+    const mul3 = (x: number) => x ^ (x & 0x80 ? (x << 1) ^ 0x11b : (x << 1)); // x * 3
 
     const exp3 = new Array<number>(256); // exp3[x] = 3**x
     const log3 = new Array<number>(256); // y = exp3[x], x = log3[y]
@@ -13,8 +13,10 @@ namespace tsumego.ff256 {
     for (let x = 0, y = 1; x < 256; x++ , y = mul3(y))
         log3[exp3[x] = y] = x;
 
-    export const mul = (a: number, b: number) => exp3[log3[a] + log3[b] & 255]; // a * b
-    export const inv = (x: number) => exp3[255 ^ log3[x]]; // x * inv(x) = 1
+    const inv1 = log3.map(x => exp3[255 ^ x]); // x * inv1[x] = 1
+
+    export const mul = (a: number, b: number) => a && b && exp3[log3[a] + log3[b] & 255];
+    export const inv = (y: number) => inv1[y];
 
     export const mul4 = (a: number, b: number) => b4(
         mul(b0(a), b0(b)),
--- tests/src/ff256.ts
+++ tests/src/ff256.ts
@@ -2,7 +2,7 @@ namespace tests {
     import ff256 = tsumego.ff256;
 
     ut.group($ => {
-        /// GF(2**8)
+        /// FF(2**8)
 
         $.test($ => {
             /// inv
@@ -16,22 +16,22 @@ namespace tests {
         $.test($ => {
             /// inv4
 
-            $(ff256.inv4(0x234617f5)).equal(0xf1f55f46);
+            $(ff256.inv4(0x234617f5)).equal(0xf1f55f46 | 0);
         });
 
         $.test($ => {
             /// mul
 
-            $(ff256.mul(0x12, 0x23)).equal(0x00);
-            $(ff256.mul(0x46, 0x11)).equal(0x00);
-            $(ff256.mul(0x18, 0x0b)).equal(0x00);
-            $(ff256.mul(0xc5, 0xd3)).equal(0x00);
+            $(ff256.mul(0xb6, 0x53)).equal(0x36);
+            $(ff256.mul(0x46, 0x11)).equal(0x4a);
+            $(ff256.mul(0x18, 0x0b)).equal(0xe8);
+            $(ff256.mul(0xc5, 0xd3)).equal(0x6c);
         });
 
         $.test($ => {
             /// mul4
 
-            $(ff256.mul4(0x124618c5, 0x23110bd3)).equal(0x00000000);
+            $(ff256.mul4(0xb64618c5, 0x53110bd3)).equal(0x364ae86c | 0);
         });
     });
 }
