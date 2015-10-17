03dd6bdbde7f783840d15cb25ea1eaf394e0ad73
d180cf <d180cf@gmail.com>
2015-10-17 16:08:57 -0700

    quaternions

--- src/ff256.ts
+++ src/ff256.ts
@@ -1,5 +1,5 @@
 /**
- * Implements the finite field GF(2**8).
+ * The finite field GF(2**8) over 0x11b.
  *
  * en.wikipedia.org/wiki/Finite_field_arithmetic
  * www.cs.utsa.edu/~wagner/laws/FFM.html
@@ -13,26 +13,43 @@ namespace tsumego.ff256 {
     for (let x = 0, y = 1; x < 256; x++ , y = mul3(y))
         log3[exp3[x] = y] = x;
 
-    const inv = log3.map(x => exp3[255 ^ x]); // x * inv1[x] = 1
+    log3[1] = 0;
 
-    const mul = (a: number, b: number) => a && b && exp3[log3[a] + log3[b] & 255];
-    const div = (a: number, b: number) => a && b && exp3[log3[a] - log3[b] & 255];
+    const invt = log3.map(x => exp3[255 - x]); // x * inv1[x] = 1
+    const cut = (x: number) => x > 255 ? x - 255 : x;
 
-    export const mul4 = (a: number, b: number) => b4(
-        mul(b0(a), b0(b)),
-        mul(b1(a), b1(b)),
-        mul(b2(a), b2(b)),
-        mul(b3(a), b3(b)));
+    export const mul = (a: number, b: number) => a && b && exp3[cut(log3[a] + log3[b])];
+    export const inv = (a: number) => invt[a];
+    export const div = (a: number, b: number) => mul(a, inv(b));
+}
 
-    export const div4 = (a: number, b: number) => b4(
-        div(b0(a), b0(b)),
-        div(b1(a), b1(b)),
-        div(b2(a), b2(b)),
-        div(b3(a), b3(b)));
+/**
+ * Quaternions in GF(2**8).
+ *
+ * en.wikipedia.org/wiki/Quaternion
+ * math.stackexchange.com/questions/7708/quaternion-division
+ */
+namespace tsumego.ff256.q {
+    import m = ff256.mul;
+    import i = ff256.inv;
 
-    export const inv4 = (x: number) => b4(
-        inv[b0(x)],
-        inv[b1(x)],
-        inv[b2(x)],
-        inv[b3(x)]);
+    export const mul = (a: number, b: number) => {
+        const [a0, a1, a2, a3] = b_(a);
+        const [b0, b1, b2, b3] = b_(b);
+
+        return b4(
+            m(a0, b0) ^ m(a1, b1) ^ m(a2, b2) ^ m(a3, b3),
+            m(a0, b1) ^ m(a1, b0) ^ m(a2, b3) ^ m(a3, b2),
+            m(a0, b2) ^ m(a1, b3) ^ m(a2, b0) ^ m(a3, b1),
+            m(a0, b3) ^ m(a1, b2) ^ m(a2, b1) ^ m(a3, b0));
+    };
+
+    export const inv = (a: number) => {
+        const [a0, a1, a2, a3] = b_(a);
+        // TODO: what if the sum is zero?
+        const s = i(m(a0, a0) ^ m(a1, a1) ^ m(a2, a2) ^ m(a3, a3));
+        return b4(m(s, a0), m(s, a1), m(s, a2), m(s, a3));
+    };
+
+    export const div = (a: number, b: number) => mul(a, inv(b));
 }
