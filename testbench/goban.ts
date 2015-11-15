d0f2720823a114ff7ee5da64e6e6d270a76540ca
d180cf <d180cf@gmail.com>
2015-11-15 03:14:48 -0800

    Improved look of the SVG goban.

--- testbench/goban.ts
+++ testbench/goban.ts
@@ -21,26 +21,25 @@ namespace testbench.gobanui {
                 const c = board.get(x, y);
                 if (!c) continue;
 
-                shapes.push(`<circle cx="${10 + x * 20}" cy="${10 + y * 20}" r="9" ${c < 0 ? 'fill="white"' : ''} stroke="black" />`);
+                shapes.push(`<use x="${x}" y="${y}" xlink:href="#${c > 0 ? 'AB' : 'AW'}"/>`);
             }
         }
 
         const markers = {
             TR: (x, y) =>
-                `<polygon points="${[x, y + 3]} ${[x - 3, y - 3]} ${[x + 3, y - 3]}" fill="red" />`,
+                `<use x="${x}" y="${y}" xlink:href="#TR"/>`,
 
             SL: (x, y) =>
-                `<polygon points="${[x - 3, y - 3]} ${[x + 3, y - 3]} ${[x + 3, y + 3]} ${[x - 3, y + 3]}" fill="red" />`,
+                `<use x="${x}" y="${y}" xlink:href="#SQ"/>`,
 
             MA: (x, y) =>
-                `<line x1="${x - 3}" y1="${y - 3}" x2="${x + 3}" y2="${y + 3}" stroke="red" />` +
-                `<line x1="${x - 3}" y1="${y + 3}" x2="${x + 3}" y2="${y - 3}" stroke="red" />`,
+                `<use x="${x}" y="${y}" xlink:href="#MA"/>`,
         };
 
         for (const mark in markers) {
             for (const s of marks[mark] || []) {
                 const [x, y] = stone.coords(s);
-                const shape = markers[mark](10 + x * 20, 10 + y * 20);
+                const shape = markers[mark](x, y);
                 shapes.push(shape);
             }
         }
@@ -48,22 +47,41 @@ namespace testbench.gobanui {
         const wrapper = document.createElement('div');
 
         wrapper.innerHTML = `
-            <svg width="100%" viewBox="0 0 ${n * 20} ${n * 20}">
-                <defs>
-                    <pattern id="grid" x="10" y="10" width="20" height="20" patternUnits="userSpaceOnUse">
-                        <path d="M 20 0 L 0 0 0 20" fill="none" stroke="black" />
-                    </pattern>
-                </defs>
+            <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:xlink="http://www.w3.org/1999/xlink"
+                 width="100%"
+                 viewBox="-0.5 -0.5 ${n} ${n}">
+              <style>
+                * { stroke-width: 0.05 }
+              </style>
+              <defs>
+                <pattern id="grid" x="0" y="0" width="1" height="1" patternUnits="userSpaceOnUse">
+                  <path d="M 1 0 L 0 0 0 1" fill="none" stroke="black"></path>
+                </pattern>
 
-                <rect x="10" y="10" width="${n * 20 - 19}" height="${n * 20 - 19}" fill="url(#grid)" />
-                ${shapes.join('\r\n') }
+                <circle id="AW" r="0.475" fill="white" stroke="black"></circle>
+                <circle id="AB" r="0.475" fill="black" stroke="black"></circle>
+                <circle id="CR" r="0.25" fill="none" stroke="red"></circle>
+                <path id="TR" d="M 0 -0.25 L -0.217 0.125 L 0.217 0.125 Z" fill="none" stroke="red"></path>
+                <path id="MA" d="M -0.25 -0.25 L 0.25 0.25 M 0.25 -0.25 L -0.25 0.25" fill="none" stroke="red"></path>
+                <path id="SQ" d="M -0.25 -0.25 L 0.25 -0.25 L 0.25 0.25 L -0.25 0.25 Z" fill="none" stroke="red"></path>
+              </defs>
+
+              <rect x="0" y="0" width="${n - 1}" height="${n - 1}" fill="url(#grid)" stroke="black" stroke-width="0.1"></rect>
+
+              ${shapes.join('\r\n') }
             </svg>`;
 
-        return Object.assign(wrapper.querySelector('svg'), {
+        const goban = <SVGElement>wrapper.querySelector('svg');
+
+        return Object.assign(goban, {
             getStoneCoords(offsetX: number, offsetY: number) {
+                const x = offsetX / goban.clientWidth;
+                const y = offsetY / goban.clientHeight;
+
                 return [
-                    Math.round((offsetX - 10) / 35),
-                    Math.round((offsetY - 10) / 35)
+                    Math.round(x * n - 0.5),
+                    Math.round(y * n - 0.5)
                 ];
             }
         });
