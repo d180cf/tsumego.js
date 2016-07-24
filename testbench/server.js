0644acf5a83ca86fe1e18eee76e009044a6b0a5b
d180cf <d180cf@gmail.com>
2016-07-24 00:07:30 -0700

    Removed the homebrew http server.
    npm i -g http-server is just good enough.

--- testbench/server.js
+++
@@ -1,7 +0,0 @@
-﻿var path = require('path');
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = +process.argv[2] || 8080;
-var dir = path.join(__dirname, '..')
-connect().use(serveStatic(dir)).listen(port);
-console.log('Serving', dir, 'from port', port);
