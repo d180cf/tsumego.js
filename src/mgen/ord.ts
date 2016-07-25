25c9b9a4a9d706e2f8daf62a9d1f641500437e54
d180cf <d180cf@gmail.com>
2016-07-25 01:47:31 -0700

    Map values to the [0, 1] range.

--- src/mgen/ord.ts
+++ src/mgen/ord.ts
@@ -1,4 +1,10 @@
 module tsumego.mgen {
+    // this is something like the sigmoid function
+    // to map values to [0, 1] range, but it's
+    // considerably faster; it's derivative is
+    // dS / dx = ((2 * S - 1)/x)**2
+    const S = (x: number) => (1 + x / (1 + sign(x) * x)) / 2;
+
     export class MvsOrd {
         /** Defines the order in which the solver considers moves. */
         sa = new SortedArray<stone>();
@@ -33,28 +39,28 @@ module tsumego.mgen {
                 // it's surprising, that with this dumb moves ordering
                 // and with the cached tt results, the 1-st move appears
                 // to be wrong only in 2% of cases; weights for these
-                // parameters were guessed - perhaps there are better ones
+                // parameters were guessed there must be better ones
                 this.sa.insert(s, [
                     // maximize the number of captured stones first
-                    + 1e-1 * r
+                    + 1e-0 * S(r)
 
                     // minimize the number of own blocks in atari
-                    + 1e-2 * -ninatari(board, +color)
+                    + 1e-1 * S(-ninatari(board, +color))
 
                     // minimize/maximize the number of libs of the target
-                    + 1e-3 * n * color * sign(t)
+                    + 1e-2 * S(n * color * sign(t))
 
                     // maximize the number of own liberties
-                    + 1e-4 * sumlibs(board, +color)
+                    + 1e-3 * S(sumlibs(board, +color))
 
                     // maximize the number of the opponent's blocks in atari
-                    + 1e-5 * ninatari(board, -color)
+                    + 1e-4 * S(ninatari(board, -color))
 
                     // minimize the number of the opponent's liberties
-                    + 1e-6 * -sumlibs(board, -color)
+                    + 1e-5 * S(-sumlibs(board, -color))
 
                     // if everything above is the same, pick a random move
-                    + 1e-7 * (random() - 0.5)
+                    + 1e-6 * S(random() - 0.5)
                 ]);
             } finally {
                 board.undo();
