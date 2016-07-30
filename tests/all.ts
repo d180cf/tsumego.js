0ce654b5af9b39f98ab885a69f4aa7fba63c6280
d180cf <d180cf@gmail.com>
2016-07-30 14:58:15 -0700

    moved the list of files to tsconfig.json

--- tests/all.ts
+++ tests/all.ts
@@ -1,28 +1,3 @@
-/// <reference path="../tsumego.d.ts" />
-/// <reference path="../libs/node.d.ts" />
-
-/// <reference path="isnode.ts" />
-/// <reference path="colors.ts" />
-/// <reference path="argv.ts" />
-/// <reference path="infra.ts" />
-/// <reference path="console.tree.ts" />
-/// <reference path="stats.ts" />
-/// <reference path="../libs/json-file.ts" />
-/// <reference path="collection.ts" />
-/// <reference path="es6aiter.ts" />
-/// <reference path="ascii-board.ts" />
-
-/// <reference path="src/dist.ts" />
-/// <reference path="src/fixed.ts" />
-/// <reference path="src/utils.ts" />
-/// <reference path="src/gf2.ts" />
-/// <reference path="src/sgf.ts" />
-/// <reference path="src/dcnn.ts" />
-/// <reference path="src/board.ts" />
-/// <reference path="src/dumb.ts" />
-/// <reference path="src/search.ts" />
-/// <reference path="src/benson.ts" />
-
 namespace tests {
     console.log('\nTotal:', ((Date.now() - _dt0) / 1000).toFixed(1).white() + 's');
 
