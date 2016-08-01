c258006b84418558251fb861385fdf1829b5e5b2
d180cf <d180cf@gmail.com>
2016-08-01 15:54:31 -0700

    vm as a class instance

--- app/vm.ts
+++ app/vm.ts
@@ -17,12 +17,12 @@ module testbench {
     hookToolToKey('AB', 'B'); // B = black
     hookToolToKey('AW', 'W'); // W = white
 
-    export const vm = {
+    export const vm = new class VM {
         /** The currently selected editor tool: MA, AB, AW, etc. */
         get tool(): string {
             const button = document.querySelector('#tool button.active');
             return button && button.getAttribute('data-value');
-        },
+        }
 
         set tool(value: string) {
             for (const button of $('#tool button').toArray()) {
@@ -31,13 +31,13 @@ module testbench {
                 else
                     button.classList.remove('active');
             }
-        },
+        }
 
         /** ko master: +1, -1 or 0 */
         get km(): number {
             const b = document.querySelector('#km button.active');
             return b ? +b.getAttribute('data-value') : undefined;
-        },
+        }
 
         set km(value: number) {
             for (const button of $('#km button').toArray()) {
@@ -46,42 +46,42 @@ module testbench {
                 else
                     button.classList.remove('active');
             }
-        },
+        }
 
         /** Hides/shows the km selector. */
         set kmVisible(viisble: boolean) {
             $('#km').css('display', viisble ? '' : 'none');
-        },
+        }
 
         /** e.g. "B3 bc" */
         set coords(text: string) {
             $('#coords').text(text);
-        },
+        }
 
         set note(text: string) {
             $('#comment').text(text);
-        },
+        }
 
         get sgf(): string {
             return $('#sgf').text();
-        },
+        }
 
         set sgf(text: string) {
             $('#sgf').text(text);
-        },
+        }
 
         set svg(text: string) {
             $('#svg').text(text);
-        },
+        }
 
         set canUndo(value: boolean) {
             if (value)
                 $('#undo').removeClass('disabled');
             else
                 $('#undo').addClass('disabled');
-        },
+        }
 
-        dbg: {
+        dbg = new class DbgVM {
             set enabled(value: boolean) {
                 if (value) {
                     $('#footer').css('background-color', '#a7691c');
@@ -91,39 +91,39 @@ module testbench {
                 } else {
                     debugger;
                 }
-            },
+            }
 
             set inactive(value: boolean) {
                 $('#dbg-panel button').toggleClass('disabled', value);
-            },
+            }
 
             /** aka F5 */
             get run() {
                 return $('#dbg-run');
-            },
+            }
 
             get bp() {
                 return $('#dbg-bp');
-            },
+            }
 
             /** aka F11 */
             get stepInto() {
                 return $('#dbg-into');
-            },
+            }
 
             /** aka F10 */
             get stepOver() {
                 return $('#dbg-next');
-            },
+            }
 
             /** aka Shift+F11 */
             get stepOut() {
                 return $('#dbg-undo');
-            },
+            }
 
             get stop() {
                 return $('#dbg-stop');
-            },
-        }
+            }
+        };
     };
 }
