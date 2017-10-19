e715834ad490400a6ed14e76711c7412c20c6af5
d180cf <d180cf@gmail.com>
2017-10-18 20:05:23 -0700

    Fixed GH_TOKEN

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e # exit with nonzero exit code if anything fails
 
 if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
 
-echo "Starting to update ${GH_REPO}\n"
+echo "Starting to update ${GH_REPO}"
 
 #copy data we're interested in to other place
 cp -R bin/site $HOME/site
@@ -15,7 +15,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
 #using token clone master branch
-git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git repo > /dev/null
+git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git repo
 
 #go into directory and copy data we're interested in to that directory
 cd repo
@@ -26,7 +26,7 @@ git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
 git push > /dev/null
 
-echo "Done updating ${GH_REPO}\n"
+echo "Done updating ${GH_REPO}"
 
 else
  echo "Skipped updating ${GH_REPO}, because build is not triggered from the master branch."
